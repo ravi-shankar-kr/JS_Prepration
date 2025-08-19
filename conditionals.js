4c5515a822879a4f9ad83e3d813e0a346b3100b9
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-08-19 23:31:37 +0530

    initial commit

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+let age = 20;
+if (age >= 18) {
+    console.log("You are an adult.");
+}
+else{
+    console.log("You are a minor.");
+}
+
+
+// odd even
+let num = 20;
+if (num % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
---
+++ loops.js
@@ -0,0 +1,12 @@
+// for ;oops
+for (let i = 0; i<=10; i++){
+    console.log(i);
+}
+console.log();
+
+// while loops
+let i = 0;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
